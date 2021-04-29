b301599e1ddf60bffebf844caf29d4810f8a0782
Sangpil Jung <59302973+Thinkteam20@users.noreply.github.com>
2021-04-29 22:36:57 +1000

    finding Minimum Distance inside of given array

--- MinDistance/MinDistance/Program.cs
+++ MinDistance/MinDistance/Program.cs
@@ -6,7 +6,25 @@ namespace MinDistance
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] num = { 1, 10, 4, 6 };
+            int results = findDistance(num);
+            Console.WriteLine("Minimum Distance is {0:d}", results);
+        }
+        static int findDistance (int[] A)
+        {
+            int dmin = 10000;
+            for(int i = 0; i <= A.Length - 2; i++)
+            {
+                for (int j = i + 1; j <= A.Length - 1; j++)
+                {
+                    int temp = Math.Abs(A[i] - A[j]);
+                    if (temp < dmin)
+                    {
+                        dmin = temp;
+                    }
+                }
+            }
+            return dmin;
         }
     }
 }
