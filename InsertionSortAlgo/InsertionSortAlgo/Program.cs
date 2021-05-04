a3924b163cca009ad99a1ef6b118cdf120b68f4f
Sangpil Jung <59302973+Thinkteam20@users.noreply.github.com>
2021-05-04 22:28:43 +1000

    added InsertionSortedAlgo

---
+++ InsertionSortAlgo/InsertionSortAlgo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace InsertionSortAlgo
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[] sorted_array = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int sorted_array_length = sorted_array.Length;
+
+        }
+
+        static void InsertionSort(int []A, int n)
+        {
+            for(int i = 1; i < n - 1; i++)
+            {
+                int v = A[i];
+                int j = i - 1;
+                while(j >= 0 && A[j] > v)
+                {
+                    A[j + 1] = A[j];
+                    j = j - 1;
+                }
+                A[j + 1] = v;
+            }
+        }
+    }
+}
